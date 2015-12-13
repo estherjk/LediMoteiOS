a38dc9e11226649e371ad1c192e839fdb1fb5b07
Esther Jun Kim <esther.jun.kim@gmail.com>
2015-12-13 15:36:13 -0800

    Add ability to sync LED states with other clients

--- LediMoteiOS/LediMoteViewController.swift
+++ LediMoteiOS/LediMoteViewController.swift
@@ -27,6 +27,52 @@ class LediMoteViewController: UITableViewController {
         
         socket.on("connect") { data, ack in
             print("socket connected")
+            
+            self.socket.emit("hello")
+        }
+        
+        socket.on("init") { data, ack in
+            print(data)
+            
+            if let state = data[0]["red"] as? Bool {
+                self.redSwitch.setOn(state, animated: true)
+            }
+            
+            if let state = data[0]["green"] as? Bool {
+                self.greenSwitch.setOn(state, animated: true)
+            }
+            
+            if let state = data[0]["blue"] as? Bool {
+                self.blueSwitch.setOn(state, animated: true)
+            }
+            
+            if let state = data[0]["yellow"] as? Bool {
+                self.yellowSwitch.setOn(state, animated: true)
+            }
+        }
+        
+        socket.on("red") { data, ack in
+            if let state = data[0]["state"] as? Bool {
+                self.redSwitch.setOn(state, animated: true)
+            }
+        }
+        
+        socket.on("green") { data, ack in
+            if let state = data[0]["state"] as? Bool {
+                self.greenSwitch.setOn(state, animated: true)
+            }
+        }
+        
+        socket.on("blue") { data, ack in
+            if let state = data[0]["state"] as? Bool {
+                self.blueSwitch.setOn(state, animated: true)
+            }
+        }
+        
+        socket.on("yellow") { data, ack in
+            if let state = data[0]["state"] as? Bool {
+                self.yellowSwitch.setOn(state, animated: true)
+            }
         }
         
         socket.connect()
